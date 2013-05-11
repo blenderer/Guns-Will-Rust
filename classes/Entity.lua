cb050a137628c3b3ca7e64d0adb976df667027df
Eric Harrison <blendererer@gmail.com>
2013-05-11 14:28:53 -0400

    added debug functionality

--- classes/Entity.lua
+++ classes/Entity.lua
@@ -1,5 +1,28 @@
 Entity = class('Entity')
 
 function Entity:initialize(xpos, ypos)
+	self.x = xpos
+	self.y = ypos
+	self.hp = 1
+end
 
+function Entity:getX()
+	return self.x
+end
+
+function Entity:getX()
+	return self.x
+end
+
+function Entity:getHp()
+	return self.hp
+end
+
+function Entity:kill()
+	self.hp = 0
+end
+
+function Entity:move(xpos, ypos)
+	self.x = xpos
+	self.y = ypos
 end
--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 require 'libraries/libraries'
-require '/classes/classes'
+require 'classes/classes'
 
 function love.load()
 	
@@ -11,5 +11,12 @@ end
 
 
 function love.draw()
-	
+	love.graphics.print( "hello world", 0, 0)
+end
+
+function love.keypressed(key, u)
+	--Debug
+	if key == "rctrl" then
+		debug.debug()
+	end
 end
